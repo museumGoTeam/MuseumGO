ca5ce4383cf0cfaf1fed046488fe6e845fe0f0c6
MuseumGoTeam <museumgo.team@gmail.com>
2020-12-05 15:03:08 +0100

    scan poi and redirect to the poi details

--- atomic/pages/PoiScanP.tsx
+++ atomic/pages/PoiScanP.tsx
@@ -1,15 +1,31 @@
+import { useNavigation } from '@react-navigation/native'
 import React from 'react'
 import { StyleSheet, View } from 'react-native'
 import { useTheme } from 'react-native-paper'
-import Typography from '../atoms/Typography'
+import { useSelector } from 'react-redux'
+import { IAppState } from '../../store/types'
+import Message from '../atoms/Snackbar'
+import QRCodeScanT from '../templates/QRCodeScanT'
 
 export default function PoiScanP() {
-    const theme = useTheme()
+    const theme = useTheme()    
     const styles = useStyles(theme)
+    const navigation = useNavigation()
+    const poiSelected = useSelector((state: IAppState) => state.poiSelected)
+
+    const onPoIScan = (poiID: string) => {
+        if (poiSelected) {
+            if (poiSelected._id !== poiID) {
+                Message.error("You didn't select this point of interest")
+            } else {
+                navigation.navigate("PoiDetails")
+            }
+        }
+    }
 
     return (
         <View style={styles.root}>
-            <Typography>Poi Scan P</Typography>
+            <QRCodeScanT onScanned={onPoIScan}  />
         </View>
     )
 }
@@ -18,7 +34,5 @@ export default function PoiScanP() {
 const useStyles = (theme: ReactNativePaper.Theme) => StyleSheet.create({
     root: {
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
     }
 })
